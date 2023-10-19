4d39d197efd2ecc8ee9cf9b2b8e2fa266fed3519
D4n1Fr4nc0 <127441164+D4n1Fr4nc0@users.noreply.github.com>
2023-10-19 16:26:37 -0500

    Add files via upload

---
+++ Json - Boostrap_D-R-F/data/data.js
@@ -0,0 +1,44 @@
+export const data =
+[
+{
+"id": 1,
+"name": "luna",
+"image": "/img/imagen1.jpg"
+},
+{
+"id": 2,
+"name": "luna",
+"image": "/img/imagen2.jpg"
+},
+{
+"id": 3,
+"name": "luna",
+"image": "/img/imagen3.jpg"
+},
+{
+"id": 4,
+"name": "luna",
+"image": "/img/imagen4.jpg"
+},
+{
+"id": 5,
+"name": "luna",
+"image": "/img/imagen5.jpg"
+},
+{
+"id": 6,
+"name": "luna",
+"image": "/img/imagen6.jpg"
+},
+{
+"id": 7,
+"name": "luna",
+"image": "/img/imagen7.jpg"
+},
+{
+ "id": 8,
+"name": "luna",
+"image": "/img/imagen8.jpg"
+},
+    
+]
